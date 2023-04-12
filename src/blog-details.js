009ef477b54b276f11816560939b9c74edae789d
Sofie Tønder <105886175+sofietoender@users.noreply.github.com>
2023-04-12 20:37:32 +0200

    added details

--- src/blog-details.js
+++ src/blog-details.js
@@ -1,26 +1,29 @@
-const postsContainer = document.querySelector(".post-container");
+const detailsHeader = document.querySelector(".blog-header");
+const detailsContent = document.querySelector(".blog-post");
 
-async function fetchPosts() {
+const queryString = document.location.search;
+const params = new URLSearchParams(queryString);
+const id = params.get("id");
+
+const url = `https://ca-cms-sofie.flywheelsites.com/wp-json/wp/v2/posts/${id}`;
+
+async function fetchBlogDetails() {
   try {
-    const response = await fetch('https://ca-cms-sofie.flywheelsites.com/wp-json/wp/v2/posts/');
-    const posts = await response.json();
-    showPosts(posts);
+    const response = await fetch(url);
+    const blogDetails = await response.json();
+
+    showBlogDetails(blogDetails);
   } catch (error) {
     console.log(error);
   }
 }
 
-function showPosts(posts) {
-  posts.forEach(post => {
-    const postDiv = document.createElement('div');
-    const postLink = `blog-details.html?id=${post.id}`;
-    postDiv.innerHTML = `
-      <a href="${postLink}"><h2>${post.title.rendered}</h2></a>
-      <p>By ${post.author} on ${new Date(post.date).toLocaleDateString()}</p>
-      <p>${post.excerpt.rendered}</p>
-    `;
-    postsContainer.appendChild(postDiv);
-  });
+function showBlogDetails(blogDetails) {
+  const title = blogDetails.title.rendered;
+  const content = blogDetails.content.rendered;
+
+  detailsHeader.innerHTML = `<h1>${title}</h1>`;
+  detailsContent.innerHTML = content;
 }
 
-fetchPosts();
+fetchBlogDetails();
